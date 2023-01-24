953c4973ad14ee6d4ab2f7c7550cbdc32ee0901b
Manu Kaushik <codebymanuk@gmail.com>
2023-01-24 13:26:48 +0530

    methods

---
+++ Basics/main.java
@@ -0,0 +1,22 @@
+// Methods in Java
+
+public class main {
+
+  public static void main(String[] args) {
+    Computer obj = new Computer();
+    obj.playmusic();
+    String str = obj.getmepen(20);
+    System.out.println(str);
+  }
+}
+
+class Computer {
+
+  public void playmusic() {
+    System.out.println("Music Playing....");
+  }
+
+  public String getmepen(int cost) {
+    return "Pen";
+  }
+}
