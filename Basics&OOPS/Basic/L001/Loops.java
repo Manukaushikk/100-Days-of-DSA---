6c358d470365ab4679259f30a73ffb2225e10d51
Manu Kaushik <codebymanuk@gmail.com>
2023-01-26 00:37:56 +0530

    loops✔️

---
+++ Basics&OOPS/Basic/L001/Loops.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+// import java.util.*;
+
+public class Loops {
+
+  public static void main(String[] args) {
+    // Scanner scn = new Scanner(System.in);
+    // // int x = scn.nextInt();
+
+    // int i = 1;
+    // while (i <= 9) {
+    //   System.out.println(i);
+    //   i++;
+    // }
+    // System.out.println("It's DONE");
+
+    // ⬇️ Infinite Loop ⬇️
+
+    // while (true) {
+    //     System.out.println("Manu Kaushik 😂");
+    // // }
+
+    // for (;;) {
+    //   System.out.println("Manu Kaushik 😂");
+    // }
+
+    // do {
+    //   System.out.println("Manu Kaushik 😂");
+    // } while (true);
+  }
+}
