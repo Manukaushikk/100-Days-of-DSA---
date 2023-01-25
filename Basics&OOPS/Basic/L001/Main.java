a13cd40e79034fe41c6597f2d4908f9922a9b520
Manu Kaushik <codebymanuk@gmail.com>
2023-01-25 23:13:02 +0530

    if and else

--- Basics&OOPS/Basic/L001/Main.java
+++ Basics&OOPS/Basic/L001/Main.java
@@ -13,15 +13,15 @@ public class Main {
 
   public static void main(String[] args) {
     Scanner scn = new Scanner(System.in);
-    int n = scn.nextInt();
+    int marks = scn.nextInt();
 
-    if (n > 90) {
+    if (marks > 90) {
       System.out.println("excellent");
-    } else if (n > 80 && n < 90) {
+    } else if (marks > 80 && marks < 90) {
       System.out.println("good");
-    } else if (n > 70 && n < 80) {
+    } else if (marks > 70 && marks < 80) {
       System.out.println("fair");
-    } else if (n > 60 && n < 70) {
+    } else if (marks > 60 && marks < 70) {
       System.out.println("meets expectations");
     } else {
       System.out.println("below par");
