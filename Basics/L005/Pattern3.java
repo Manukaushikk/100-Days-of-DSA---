542817fa6305b731665c6e8e69e96e8895507332
Manu Kaushik <codebymanuk@gmail.com>
2023-01-28 01:10:35 +0530

    Pattern4✔️

--- Basics/L005/Pattern3.java
+++ Basics/L005/Pattern3.java
@@ -1,4 +1,26 @@
+import java.util.Scanner;
 
 public class Pattern3 {
-    
+
+  public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+
+    int space = n - 1;
+    int star = 1;
+
+    for (int i = 1; i <= n; i++) {
+      //   System.out.println(space + " * " + star);
+      for (int j = 1; j <= space; j++) {
+        System.out.print("\t");
+      }
+
+      for (int j = 1; j <= star; j++) {
+        System.out.print("*\t");
+      }
+      space--;
+      star++;
+      System.out.println();
+    }
+  }
 }
