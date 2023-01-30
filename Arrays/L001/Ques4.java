b738cb02e9b5c3164d61ccc613a0b470d3931de5
Manu Kaushik <codebymanuk@gmail.com>
2023-01-30 11:28:03 +0530

    Day 1 ✅

--- Arrays/L001/Ques4.java
+++ Arrays/L001/Ques4.java
@@ -1,3 +1,8 @@
+// Reverse An Array
+
+// 1. You are given a number n, representing the size of array a.
+// 2. You are given n numbers, representing elements of array a.
+// 3. You are required to reverse the contents of array a.
 import java.util.Scanner;
 
 public class Ques4 {
