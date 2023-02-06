8eeefcaf2e0a3c4ec98f3e9994a29c7f72ea8b7b
Manu Kaushik <codebymanuk@gmail.com>
2023-02-06 23:58:00 +0530

    Ques1✔️

---
+++ String/L001/Practice.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Practice {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String n = scn.nextLine();
+        // System.out.println(n.substring(1, 3));
+        // System.out.println(n.substring(1));
+        // System.out.println(n.charAt(0));
+        System.out.println(4 + n + 9);
+
+    }
+}
--- String/L001/Ques1.java
+++ String/L001/Ques1.java
@@ -1,5 +1,38 @@
+import java.util.Scanner;
+
+// Pallindrome Strings checking
 public class Ques1 {
     public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String str = scn.nextLine();
+        PrintAllPallindromeStrings(str);
 
     }
+
+    public static void PrintAllPallindromeStrings(String str) {
+        for (int sidx = 0; sidx < str.length(); sidx++) {
+            for (int eidx = sidx + 1; eidx <= str.length(); eidx++) {
+                String ss = str.substring(sidx, eidx);
+                if (isPallindrome(ss)) {
+                    System.out.println(ss);
+                }
+            }
+        }
+    }
+
+    public static boolean isPallindrome(String str) {
+        int left = 0, right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+
+            }
+            left++;
+            right--;
+        }
+        return true;
+
+    }
+
 }
