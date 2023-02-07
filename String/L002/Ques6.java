9e6c1c0e287a57a588ada7d4450ffbfe88a0f4cc
Manu Kaushik <codebymanuk@gmail.com>
2023-02-07 21:07:18 +0530

    Ques6✔️

--- String/L002/Ques6.java
+++ String/L002/Ques6.java
@@ -1,5 +1,22 @@
-public class Ques6 {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+// Squares of a Sorted Array
+
+class Ques6 {
+    public int[] sortedSquares(int[] nums) {
+        int p1 = 0, p2 = nums.length - 1;
+        int arr[] = new int[nums.length];
+        int idx = nums.length - 1;
+        while (p1 <= p2) {
+            if (nums[p1] * nums[p1] >= nums[p2] * nums[p2]) {
+                arr[idx] = nums[p1] * nums[p1];
+                p1++;
+            } else {
+                arr[idx] = nums[p2] * nums[p2];
+                p2--;
+            }
+            idx--;
+        }
+        return arr;
     }
 }
