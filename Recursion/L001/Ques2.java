ec44d0ccb8971376da4a66994fd352247a2d0257
Manu Kaushik <codebymanuk@gmail.com>
2023-02-08 20:01:28 +0530

    Ques2✔️

---
+++ Recursion/L001/Ques2.java
@@ -0,0 +1,19 @@
+
+// Print Decreasing 🔻
+import java.util.Scanner;
+
+public class Ques2 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        printDecreasing(n);
+    }
+
+    public static void printDecreasing(int n) {
+        if (n == 0)
+            return;
+
+        System.out.println(n);
+        printDecreasing(n - 1);
+    }
+}
