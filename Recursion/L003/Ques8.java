276e42f08201d6de8c99725718623c280387e1e5
Manu Kaushik <codebymanuk@gmail.com>
2023-02-11 23:56:28 +0530

    Ques8✔️

---
+++ Recursion/L003/Ques8.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+// Display Reverse Array
+
+public class Ques8 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = scn.nextInt();
+
+        DisplayReverseArr(arr, 0);
+
+    }
+
+    public static void DisplayReverseArr(int arr[], int idx) {
+        if (idx == arr.length)
+            return;
+        DisplayReverseArr(arr, idx + 1);
+        System.out.println(arr[idx]);
+    }
+}
