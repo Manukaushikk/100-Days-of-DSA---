b76e6a9c43d6af03392ccfc0dd3f15cae2016506
Manu Kaushik <codebymanuk@gmail.com>
2023-02-14 13:45:50 +0530

    Ques14✔️

---
+++ Recursion/L005/Ques14.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Ques14 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String str = scn.nextLine();
+
+        printSS(str, "");
+    }
+
+    public static void printSS(String qsf, String asf) {
+        if (qsf.length() == 0) {
+            System.out.println(asf);
+            return;
+        }
+        char ch = qsf.charAt(0);
+        String roq = qsf.substring(1);
+        printSS(roq, asf + ch);
+        printSS(roq, asf);
+    }
+
+}
