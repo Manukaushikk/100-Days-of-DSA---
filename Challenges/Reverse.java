6bf9df41e8160d3c5b57f39d0db39acdd82a1501
Manu Kaushik <codebymanuk@gmail.com>
2023-02-18 01:29:32 +0530

    reverse

---
+++ Challenges/Reverse.java
@@ -0,0 +1,18 @@
+// public class Reverse {
+// // class Solution {
+// public Node reverseList(Node head) {
+// Node prev = null;
+// Node curr = head;
+
+// while (curr != null) {
+
+// Node nbr = curr.next;
+// curr.next = prev;
+
+// prev = curr;
+// curr = nbr;
+
+// }
+// return prev;
+// }
+// }
