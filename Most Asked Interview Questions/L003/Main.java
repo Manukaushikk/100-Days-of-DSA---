59fb7cfe5d24d63a1ddce985b5cb1d9e608ef892
Manu Kaushik <codebymanuk@gmail.com>
2023-02-27 03:09:52 +0530

    Write a code to find Find the ASCII value of a character

--- Most Asked Interview Questions/L003/Main.java
+++ Most Asked Interview Questions/L003/Main.java
@@ -1,4 +1,15 @@
+import java.util.Scanner;
+
+// Write a code to find Find the ASCII value of a character
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
 
+        char c = scn.next().charAt(0);
+
+        int i = c;
+
+        System.out.println("ASCII Value of " + c + "is" + i);
+    }
 }
