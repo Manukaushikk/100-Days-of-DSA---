c04da6c5616832904e38efa0a44436b53ff107ab
Manu Kaushik <codebymanuk@gmail.com>
2023-02-27 02:54:48 +0530

    Interview Question

--- Most Asked Interview Questions/L001/Main.java
+++ Most Asked Interview Questions/L001/Main.java
@@ -1,4 +1,14 @@
+// Write a code to find Fibonacci Series using Recursion
 
 public class Main {
+    public static void main(String[] args) {
+        int n = 4;
+        System.out.println(Fibonacci(n));
+    }
 
+    public static int Fibonacci(int n) {
+        if (n <= 1)
+            return n;
+        return Fibonacci(n - 1) + Fibonacci(n - 2);
+    }
 }
