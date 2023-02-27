b3cdf5526d73e6dc6548d1703208a831b716dbdd
Manu Kaushik <codebymanuk@gmail.com>
2023-02-27 13:07:13 +0530

    Write code of Greatest Common Divisor and LCM

--- Most Asked Interview Questions/L005/Main.java
+++ Most Asked Interview Questions/L005/Main.java
@@ -1,4 +1,20 @@
+import java.util.Scanner;
+
+// Write the code to find the Fibonacci series upto the nth term.
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
 
+        int first = 0, second = 1;
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(first + " ");
+
+            int third = first + second;
+            first = second;
+            second = third;
+        }
+    }
 }
