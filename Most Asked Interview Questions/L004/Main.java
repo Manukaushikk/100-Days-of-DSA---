a8629b2b47fa9a464854fae7bd4f12e0d8f0ac5b
Manu Kaushik <codebymanuk@gmail.com>
2023-02-27 12:46:27 +0530

    Write a code to reverse a number

--- Most Asked Interview Questions/L004/Main.java
+++ Most Asked Interview Questions/L004/Main.java
@@ -1,4 +1,22 @@
+import java.util.Scanner;
+
+// Write a code to reverse a number
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        int number = scn.nextInt();
+        int reverse = 0;
+
+        String str = "";
+
+        while (number != 0) {
+            int pick_last = number % 10;
+            str = str + Integer.toString(pick_last);
+            number = number / 10;
+        }
+        System.out.println(str);
+    }
 
 }
