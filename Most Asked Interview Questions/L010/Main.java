745c4626df9d8b8c87d2c2183df1d7210a8181e8
Manu Kaushik <codebymanuk@gmail.com>
2023-02-27 16:34:50 +0530

    Write code to Calculate frequency of characters in a string

--- Most Asked Interview Questions/L010/Main.java
+++ Most Asked Interview Questions/L010/Main.java
@@ -1,4 +1,20 @@
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
 
+        String str = scn.nextLine();
+        char ch = scn.next().charAt(0);
+
+        int frequency = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (ch == str.charAt(i)) {
+
+                ++frequency;
+            }
+        }
+        System.out.println(frequency);
+    }
 }
