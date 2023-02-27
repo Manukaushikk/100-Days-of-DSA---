1c581b877b1c75d0548270924a35c861f8da9a7b
Manu Kaushik <codebymanuk@gmail.com>
2023-02-28 01:10:30 +0530

    Find non-repeating characters in a string

--- Most Asked Interview Questions/L012/Main.java
+++ Most Asked Interview Questions/L012/Main.java
@@ -1,4 +1,21 @@
+import java.util.Scanner;
+
+// Find non-repeating characters in a string
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String str = scn.nextLine();
 
+        int freq[] = new int[256];
+
+        for (int i = 0; i < str.length(); i++) {
+            freq[str.charAt(i)]++;
+        }
+
+        for (int i = 0; i < 256; i++) {
+            if (freq[i] == 1)
+                System.out.print((char) i + "");
+        }
+    }
 }
