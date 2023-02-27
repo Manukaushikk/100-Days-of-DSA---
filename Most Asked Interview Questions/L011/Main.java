5437dbf4535868f55e11d237b46fb2c7b856e163
Manu Kaushik <codebymanuk@gmail.com>
2023-02-27 16:45:17 +0530

    Write to code to check whether a given year is leap year or not.

--- Most Asked Interview Questions/L011/Main.java
+++ Most Asked Interview Questions/L011/Main.java
@@ -1,4 +1,18 @@
+import java.util.Scanner;
+
+// Write to code to check whether a given year is leap year or not.
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int year = scn.nextInt();
 
+        if (year % 400 == 0) {
+            System.out.println("Leap Year");
+        } else if (year % 4 == 0 && year % 100 != 0) {
+            System.out.println("Leap Year");
+        } else {
+            System.out.println("Not a Leap Year");
+        }
+    }
 }
