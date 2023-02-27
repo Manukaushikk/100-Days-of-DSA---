9b457b5ab064d25c2325a4c472b2126420620b76
Manu Kaushik <codebymanuk@gmail.com>
2023-02-27 13:40:11 +0530

    Write code of  Perfect number

--- Most Asked Interview Questions/L007/Main.java
+++ Most Asked Interview Questions/L007/Main.java
@@ -1,4 +1,22 @@
+import java.util.Scanner;
+
+// Write code of  Perfect number
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
 
+        int sum = 0;
+        for (int i = 1; i < n; i++) {
+            if (n % i == 0)
+                sum = sum + i;
+        }
+        if (sum == n) {
+            System.out.println("Perfect Number");
+        } else {
+            System.out.println("Not a Perfect Number");
+
+        }
+    }
 }
