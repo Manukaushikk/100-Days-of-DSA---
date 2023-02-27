3cce2e99bb0823a4fdcf2fec8faf58e1902d8504
Manu Kaushik <codebymanuk@gmail.com>
2023-02-27 15:53:35 +0530

    Write code Check if the given string is Palindrome or not

--- Most Asked Interview Questions/L009/Main.java
+++ Most Asked Interview Questions/L009/Main.java
@@ -1,4 +1,19 @@
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String str = scn.nextLine();
 
+        String reverse = "";
+        str = str.toLowerCase();
+
+        for (int i = str.length() - 1; i >= 0; i--)
+            reverse = reverse + str.charAt(i);
+
+        if (str.equals(reverse))
+            System.out.println("Pallindrome");
+        else
+            System.out.println(" Not Pallindrome");
+    }
 }
