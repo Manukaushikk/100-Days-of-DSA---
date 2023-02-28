7dd16a26116d61bb55bb839a226d406eb6c70562
Manu Kaushik <codebymanuk@gmail.com>
2023-02-28 16:48:16 +0530

    Write the code to for Armstrong number

--- Most Asked Interview Questions/L014/Main.java
+++ Most Asked Interview Questions/L014/Main.java
@@ -1,4 +1,26 @@
+import java.util.Scanner;
+
+// Write the code to for Armstrong number
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
 
+        int origNum, rem;
+        int res = 0;
+
+        origNum = n;
+
+        while (origNum != 0) {
+            rem = origNum % 10;
+            res += Math.pow(rem, 3);
+            origNum /= 10;
+        }
+        if (res == n) {
+            System.out.println(n + " " + "Armstrong Number");
+        } else {
+            System.out.println(n + " " + " Not a Armstrong Number");
+        }
+    }
 }
