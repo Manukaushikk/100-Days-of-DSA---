5d47e954cc15b221016c17c533b861b1ca17cc9c
Manu Kaushik <codebymanuk@gmail.com>
2023-02-28 23:15:58 +0530

    Write a program to check whether a character is a vowel or consonant

--- Most Asked Interview Questions/L016/Main.java
+++ Most Asked Interview Questions/L016/Main.java
@@ -1,4 +1,20 @@
+import java.util.Scanner;
+
+// Write a program to check whether a character is a vowel or consonant
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        char ch = scn.next().charAt(0);
 
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+                ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
+            System.out.println("Vowels");
+        } else if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) {
+            System.out.println("Consonant");
+        } else {
+            System.out.println("Not a Alphabet");
+
+        }
+    }
 }
