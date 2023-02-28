483224f0b23dcc83684a9c730142f3378013ae2d
Manu Kaushik <codebymanuk@gmail.com>
2023-03-01 00:17:48 +0530

    Write a code to find an Automorphic number

--- Most Asked Interview Questions/L017/Main.java
+++ Most Asked Interview Questions/L017/Main.java
@@ -1,4 +1,28 @@
+import java.util.Scanner;
+
+// Write a code to find an Automorphic number
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        int n = scn.nextInt();
+        String str1 = Integer.toString(n);
+
+        int l1 = str1.length();
+
+        int square = n * n;
+
+        String str2 = Integer.toString(square);
+        int l2 = str2.length();
+
+        String str3 = str2.substring(l2 - l1);
+
+        if (str1.equals(str3)) {
+            System.out.println("Automorphic Number");
+        } else {
+            System.out.println("Not a Automorphic Number");
+        }
+    }
 
 }
