b872954b4d470ee6cdd2eda5008de8ebd722d7a4
Manu Kaushik <codebymanuk@gmail.com>
2023-02-28 16:58:15 +0530

    Write a program to find the sum of Natural Numbers using Recursion.

--- Most Asked Interview Questions/L015/Main.java
+++ Most Asked Interview Questions/L015/Main.java
@@ -1,4 +1,19 @@
+import java.util.Scanner;
+
+// Write a program to find the sum of Natural Numbers using Recursion.
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
 
+        System.out.println(getNaturalNumber(n));
+    }
+
+    public static int getNaturalNumber(int n) {
+        if (n == 0)
+            return n;
+
+        return n + getNaturalNumber(n - 1);
+    }
 }
