69b990fe2e6c7225710741da6e38eb4f4ae1f0d8
Manu Kaushik <codebymanuk@gmail.com>
2023-02-28 15:51:17 +0530

    Write a code to find the factorial of a number

--- Most Asked Interview Questions/L013/Main.java
+++ Most Asked Interview Questions/L013/Main.java
@@ -1,4 +1,23 @@
+import java.util.Scanner;
+
+// Write a code to find the factorial of a number
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
 
+        if (n >= 0) {
+            System.out.println(getfact(n));
+        } else {
+            System.out.println("Not a factorial");
+        }
+    }
+
+    public static int getfact(int n) {
+        if (n == 1 || n == 0)
+            return 1;
+
+        return n * getfact(n - 1);
+    }
 }
