64e22f8daecd57c68a7c257e1029554a38d08b60
Manu Kaushik <codebymanuk@gmail.com>
2023-02-28 23:16:32 +0530

    Write the code to find the Fibonacci series upto the nth term.

--- Most Asked Interview Questions/L005/Main.java
+++ Most Asked Interview Questions/L005/Main.java
@@ -1,6 +1,6 @@
-import java.util.Scanner;
 
 // Write the code to find the Fibonacci series upto the nth term.
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
